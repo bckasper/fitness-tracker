e763296cdee082816edbfed140a135ca5ab310d8
Ben Kasper <bckasper@gmail.com>
2022-01-08 16:14:05 -0500

    added html routes

--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
+const res = require("express/lib/response");
 const path = require("path");
 
 router.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, '../public.index.html'))
 });
+
+router.get('/exercise', (request, response)=>{
+    response.sendFile(path.join(__dirname, '../public/exercise.html'));
+})
+
+router.get('/stats', (request, response)=>{
+    response.sendFile(path.join(__dirname,'../public/stats.html'))
+})
+
+module.exports = router;
