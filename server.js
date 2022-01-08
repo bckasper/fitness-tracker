fb99fd9e82126384b5978acbbc258f80de48f75a
Ben Kasper <bckasper@gmail.com>
2022-01-08 16:02:41 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require('./routes/html.js'))
+app.use(require("./routes/api"));
+app.use(require('./routes/html'))
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
