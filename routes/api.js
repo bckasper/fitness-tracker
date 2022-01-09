fd41363556c5bcfa68904801da823706dcae2ea9
Ben Kasper <bckasper@gmail.com>
2022-01-09 14:18:59 -0500

    added first api route to get workouts

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const Workout = require('../models/workout.js')
 
 
-// router.get("/api/workouts", (request, response) => {
-//     db.Workout.find({})
-//         .then()
-//         .catch(err => {response.status(400).json(err)})
-// })
+router.get("/api/workouts", (request, response) => {
+    db.Workout.find({})
+        .then(workout =>{response.json(workout)})
+        .catch(err => {response.status(400).json(err)})
+})
