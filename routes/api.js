312124eebce58d5182194ce67e49785ddcf1aa31
Ben Kasper <bckasper@gmail.com>
2022-01-09 15:31:04 -0500

    push to heroku

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,26 @@ const router = require("express").Router();
 const Workout = require('../models/workout')
 
 // get workouts
+router.get("/api/workouts", (request, response) => {
+    Workout.aggregate([{
+        $addFields: {
+            totalDuration: {$sum: "$exercises.duration"}
+            }
+    }])
+    .then((database) => {
+        response.json(database);        
+    })
+    .catch((error) => {
+        response.status(400).json(error);
+    });
+});
 
 
 // create another workout
 router.post("/api/workouts",({body}, response) =>{
-    console.log(body)
     Workout.create(body)
-    console.log("test")
         .then(workout => {
             response.json(workout)
-            console.log(workout)
         })
         .catch((error) => {response.status(400).json(error)})
 })
