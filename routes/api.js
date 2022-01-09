eb2c1b147a60f47b7ca9d275b2a0bcb26e2532db
Ben Kasper <bckasper@gmail.com>
2022-01-09 14:23:25 -0500

    Added a post route for creating new workouts

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
-const Workout = require('../models/workout.js')
+const Workout = require('../models/workout')
 
-
+// get workouts
 router.get("/api/workouts", (request, response) => {
     db.Workout.find({})
         .then(workout =>{response.json(workout)})
         .catch(err => {response.status(400).json(err)})
+});
+
+// create another workout
+router.post('/',({body}, response) =>{
+    db.Workout.create(body)
+        .then(workout => {response.json(workout)})
 })
